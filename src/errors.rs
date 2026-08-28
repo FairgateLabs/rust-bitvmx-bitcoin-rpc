f69c43fc5a209d866d67b2ae81f3dedd578238a9
Kevin Wahle <62912263+KevinWahle@users.noreply.github.com>
2026-08-28 16:38:49 -0300

    apply fmt

--- src/errors.rs
+++ src/errors.rs
@@ -55,7 +55,7 @@ pub enum BitcoinClientError {
 
     #[error("Invalid wallet name: {wallet_name}")]
     InvalidWalletName { wallet_name: String },
-  
+
     #[error("Failed to convert PublicKey to CompressedPublicKey: {error}")]
-    FailedToConvertPublicKey{ error: String }
+    FailedToConvertPublicKey { error: String },
 }
